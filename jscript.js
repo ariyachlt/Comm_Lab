74098b2cf90649ae5448236e4e8cfcb1fd475be4
Ariya Chaloemtoem <ac6819@nyu.edu>
2020-09-30 21:47:48 +0400

    Commented on js

--- jscript.js
+++ jscript.js
@@ -1,3 +1,4 @@
+// adding an event to turn the home button white when the cursor is hovering over it and back to black when it leaves
 let homeButton = document.getElementById("home_button")
 homeButton.style.color = "black";
 homeButton.addEventListener('mouseenter', () => {
@@ -7,6 +8,7 @@ homeButton.addEventListener('mouseleave', () => {
     homeButton.style.color = "black";
 })
 
+// adding an event to turn the projects button white when the cursor is hovering over it and back to black when it leaves
 let projectsButton = document.getElementById("projects_button")
 projectsButton.style.color = "black";
 projectsButton.addEventListener('mouseenter', () => {
@@ -16,26 +18,16 @@ projectsButton.addEventListener('mouseleave', () => {
     projectsButton.style.color = "black";
 })
 
+// adding an event to toggle between the on and off status of the hamburger menu
 let hamburgerIcon = document.getElementById("imgClickAndChange")
 let displayMenuOptions = document.getElementById("display_menu")
 hamburgerIcon.addEventListener('click', () => {
+    // condition to check which image (of the hamburger menu) is displayed when click
     if(hamburgerIcon.src == "https://i.imgur.com/vVVlQlT.png"){
         hamburgerIcon.src = "https://i.imgur.com/rjH7EKR.png"
-        displayMenuOptions.style.display = "flex"
+        displayMenuOptions.style.display = "flex"                       // changing the attribute to "flex" displays the home and program buttons
     } else {
         hamburgerIcon.src = "https://i.imgur.com/vVVlQlT.png"
-        displayMenuOptions.style.display = "none"
+        displayMenuOptions.style.display = "none"                       // changing the attribute back to "none" hides the home and program buttons
     }
 })
-
-
-/*
-
-    if (displayMenuOptions.style.display == "none") {
-        displayMenuOptions.style.display = "flex";
-    } else {
-        displayMenuOptions.style.display = "none";
-    }
-
-
-*/
